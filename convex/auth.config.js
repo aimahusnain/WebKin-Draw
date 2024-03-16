38d4be5332a4623bc0f337271935e7d72d2e84e4
aimahusnain <aimahusnain@gmail.com>
2024-03-16 14:03:10 +0500

    first commit

---
+++ convex/auth.config.js
@@ -0,0 +1,17 @@
+// export default {
+//     providers: [
+//         {
+//         domain: "",
+//         applicationID: "convex"
+//         }
+//     ]
+// }
+
+export default {
+  providers: [
+    {
+      domain: "https://just-mastodon-0.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
